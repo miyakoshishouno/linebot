03d8f3aeee64877fcf72a099741fac97e748a1e6
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-25 14:12:47 +0900

    修正

--- chart.py
+++ chart.py
@@ -4,12 +4,12 @@ def judge(val,num):
     if answer == "チャート":
         result = "問1"
     elif answer == "Yes":
-        if num = 2:
+        if num == 2:
             result = "問2"
-        elif num = 3:
+        elif num == 3:
             result = "問3"
     elif ansewr == "No":
-        if num = 3:
+        if num == 3:
             result = "問3"
         elif num == 4:
             result = "問4"
