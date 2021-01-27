0f8cb0eccae831d219b2f6f688ea2952715e394e
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-27 13:20:57 +0900

    修正

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from psycopg2.extras import DictCursor
 import os
 import requests
 import chart
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -197,6 +198,10 @@ def make_button_template(question):
 
 # 日付ボタン
 def make_button_template2(label):
+    # 現在日時の取得
+    get_day = datetime.now()
+    get_date = get_day.year() + "-" + get_day.month() + 1 + "-" + get_day.day()
+    print(get_date)
     message_template = TemplateSendMessage(
         alt_text="a",
         template=ConfirmTemplate(
