46cf151abf2963645c4f43776f16b233df3c13f4
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-27 11:36:57 +0900

    修正

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def make_button_template2(label):
             text=label,
             actions=[
                 DatetimePickerAction(
-                    type = "date",
+                    type = "datetime",
                     label = "Select date",
                     data = "storeId=12345",
                     mode = "date",
