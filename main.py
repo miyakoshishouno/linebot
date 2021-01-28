4b80b18d8e2aa190f77781dfceda237735898e72
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-28 15:44:33 +0900

    修正

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ def on_postback(event):
                 else:
                     print(event.postback.data)
                     yoyaku_id = event.postback.data[3:]
-                    print(yoyaku_id)
+                    print(type(yoyaku_id))
                     del_response_message(yoyaku_id)
                     msg = "削除が完了しました。"
                     line_bot_api.reply_message(
