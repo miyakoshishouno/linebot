f9d9b7cb7131f143683c75eafa69b1afad97fa6c
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-28 13:43:24 +0900

    修正

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ def make_button_template3():
     #当日の場合
     if yoyaku_day == get_now:
         for i in range(len(time_list)):
-            if time((get_day.hour + 9).zfill(2),00,00) < time(time_list[i],00,00):
+            if time(int(str(get_day.hour + 9).zfill(2)),00,00) < time(time_list[i],00,00):
                 item_list.append(QuickReplyButton(\
                     action=PostbackAction(label= str(time_list[i]) + ":00~", data= str(time_list[i]) + ":00")))
         print(item_list)
