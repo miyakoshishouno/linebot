63012ef957210a3d0fa5ef16301315dc0d5f2215
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-28 18:26:17 +0900

    修正

--- main.py
+++ main.py
@@ -149,11 +149,13 @@ def get_response_message():
 def add_response_message(yoyaku_data):
     print(yoyaku_data)
     global user_id
+    print(user_id)
+    print(type(user_id))
     note = "ok"
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
             # cur.execute("INSERT INTO yoyaku_table VALUES((SELECT COALESCE(MAX(id),0)+1 FROM yoyaku_table WHERE user_id = %s),%s,%s,%s)",(user_id, yoyaku_data, note, user_id))
-            cur.execute("INSERT INTO yoyaku_table VALUES((SELECT COALESCE(MAX(id),0)+1 FROM yoyaku_table WHERE user_id = %s),%s,%s,%s)",("1", '2021/01/28 19:00:00', 'ok','1'))
+            cur.execute("INSERT INTO yoyaku_table VALUES((SELECT COALESCE(MAX(id),0)+1 FROM yoyaku_table WHERE user_id = %s),%s,%s,%s)",("1", yoyaku_data, note,'1'))
             conn.commit()
 
 
