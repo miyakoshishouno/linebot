76357a8de76dcdcb2199e9b58c2085a3e437de2b
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-29 14:43:16 +0900

    修正

--- main.py
+++ main.py
@@ -308,7 +308,6 @@ def button_del_kakunin():
 
 @handler.add(PostbackEvent)
 def on_postback(event):
-    global select_user_id,global yoyaku_day
     if isinstance(event, PostbackEvent):
         # if event.postback.params is not None:
         #     get_day = (event.postback.params['date'])[:4] + "/" + (event.postback.params['date'])[5:7] + "/" + (event.postback.params['date'])[8:]
