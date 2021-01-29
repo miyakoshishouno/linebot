8739854c3fa0c447110da863fa13bd7c41f1b9ad
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-01-29 11:45:45 +0900

    修正

--- main.py
+++ main.py
@@ -81,7 +81,6 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     print("ひとつめ",profile.user_id[:5])
     push_text = event.message.text
-    global select_user_id
 
     # ユーザ情報取得
     row = get_user_id(profile.user_id[:5])
@@ -90,11 +89,11 @@ def handle_message(event):
         print("ないよ0")
         add_user_id(profile.user_id[:5])
         row = get_user_id(profile.user_id[:5])
-        select_user_id = row[0][0]
     else:
         print("あるよ1")
-        select_user_id = row[0][0]
 
+    global select_user_id
+    select_user_id = row[0][0]
     print("ユーザID",select_user_id)
 
     if push_text in "予約":
