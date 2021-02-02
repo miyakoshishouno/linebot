39e19fb4d836d4f59dcb587894f316569f477332
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-02-02 11:51:48 +0900

    修正

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def update_yoyaku_phase(test_id):
 def select_phase(test_id):
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT (yoyaku_phase) FROM phase_table WHERE user_id = (%s)",(str(test_id),))
+            cur.execute("SELECT max(yoyaku_phase) FROM phase_table WHERE user_id = (%s)",(str(test_id),))
             rows = cur.fetchone()
             return rows
 
