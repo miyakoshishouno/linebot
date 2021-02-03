4c0c9f44384ab7d94a035f8fed3b52010f163f8f
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-02-03 14:42:33 +0900

    修正

--- main.py
+++ main.py
@@ -751,11 +751,13 @@ def on_postback(event):
             elif event.postback.data.startswith('del_id_'):
                     yoyaku_id = event.postback.data[7:]
                     del_response_message(yoyaku_id,test_id)
-                    msg = "削除が完了しました。"
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=msg)
-                    )
+                    label = "削除が完了しました。"
+                    msg = button_menu(label)
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    msg
+                )
 
 
             elif event.postback.data.startswith('change_id_'):
