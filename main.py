b5831dae758d6101f731baa025b8b505a42f9eb5
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-02-03 11:46:54 +0900

    修正

--- main.py
+++ main.py
@@ -791,7 +791,7 @@ def on_postback(event):
                 yoyaku_id = event.postback.data[18:]
                 print(yoyaku_id)
                 before_day = get_yoyaku_day(yoyaku_id)
-                get_time = str((before_day[0]).hour).zfill(2) +  ":" + str((before_day[0]).minute).zfill(2)
+                get_time = str((before_day[0]).hour).zfill(2) +  ":" + str((before_day[0]).minute).zfill(2) + ":00"
                 print(get_time)
                 cahange_date = get_day + " " + get_time
                 print(cahange_date)
