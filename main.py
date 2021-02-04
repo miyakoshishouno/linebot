ec4dd50dbedd6a346ec6e13a342fdaf70d0f4d49
miyakoshishouno <miyakoshi.shouno@brain-factory.jp>
2021-02-04 11:52:00 +0900

    修正

--- main.py
+++ main.py
@@ -762,7 +762,7 @@ def on_postback(event):
                 print(get_today)
                 print(cahange_date)
 
-                if datetime.datetime.strptime(get_today, "%Y/%m/%d %H:%M:%S") > datetime.datetime.strptime(cahange_date, "%Y/%m/%d %H:%M:%S"):
+                if datetime.datetime.strptime(get_today, "%Y/%m/%d %H:%M:%S") >= datetime.datetime.strptime(cahange_date, "%Y/%m/%d %H:%M:%S"):
                     label = "過去の時刻に設定されているため、時刻を変更してください。\n変更前予約時刻：" + str(before_day[0].hour).zfill(2) + ":00~"
                     msg = change_button_yoyaku_time(get_day,yoyaku_id)
 
